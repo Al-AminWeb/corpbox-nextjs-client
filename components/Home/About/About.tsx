078edede7beec1c4837293f1e4030801e0b82832
Al-AminWeb <mdalaminweb.1@gmail.com>
2026-01-14 23:53:49 +0600

    Add content to About component for enhanced information display

--- components/Home/About/About.tsx
+++ components/Home/About/About.tsx
@@ -16,6 +16,12 @@ export default function About() {
                         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg md:ml-4 w-[200px] h-[200px] md:w-[350px] md:h-[350px]"
                     />
                 </div>
+                <div>
+                    <h1 className="text-blue-700 text-lg font-medium tracking-widest">About Us</h1>
+                    <h1 className="text-3xl md:text-5xl font-bold mt-4"> Innovation solutions for digital success</h1>
+                    <p className="ml-0 md:ml-12 md:mt-10 mt-5 text-black text-sm md:text-base font-semibold leading-loose">Payment solutions enable businesses to accept payments Payment solutions enable businesses to accept payments from customers conveniently securely from customers conveniently and securely.</p>
+                </div>
+
             </div>
         </div>
     );
